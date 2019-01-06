37ca9dbf82d69eafcdc99d3e6627a3241f8271a0
Prince Sonnenberg <princesonnenberg@users-macbook-pro.local>
2019-01-06 15:47:37 +0200

    Change file structure to MVC design pattern

--- Do it/Controllers/DOITVC.swift
+++ Do it/Controllers/DOITVC.swift
@@ -11,66 +11,90 @@ import UIKit
 class DoITViewController: UITableViewController  {
     
     // create demo tableview data
+   
     
-   var itemArray  = ["Buy Dog food", "Create Website", "Call Doug"]
+   var itemArray  = [Item]()
  
     let defaults = UserDefaults.standard
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
 
-        if let items = defaults.array(forKey: "TodoListArray") as? [String]   {
+        let newItem1 = Item()
+        newItem1.title = "Catch Mike"
+        itemArray.append(newItem1)
+
+        
+        let newItem2 = Item()
+        newItem2.title = "Kill Mike"
+        itemArray.append(newItem2)
+
+        
+       if let items = defaults.array(forKey: "TodoListArray") as? [Item]   {
             
-            itemArray = items
-            
+       itemArray = items
+        
         }
-      
-    }
+        
+}
     
+
     //MARK - 1. Create TableView Data Source Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return itemArray.count
         
+        }
+  
+        
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+       
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+    
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+    
+        //Ternary operator==>
+        // value = condition ? valueiftrue : valueiffalse
+        
+        cell.accessoryType = item.done == true ? . checkmark : .none
+        
+        
+//        if item.done ==  true {
+//            cell.accessoryType = .checkmark
+//
+//        } else {
+//            cell.accessoryType = .none
+//
+//        }
+//
+            return cell
     }
     
-override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    
-    let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-    
-    cell.textLabel?.text = itemArray[indexPath.row]
-    
-    return cell
-    
-    
-    }
 
-    
     // MARK -2. Create Tableview Delegate Methods (to detect which row is selected using didselect) Then we establish initial cell behaviour
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
        
-        // print(itemArray[indexPath.row]) // gets item out of hardcoded array data
         
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        //implement checkmark in code as opposed do default checks via settings
-       
+ 
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
+        tableView.reloadData()
         // highlight selection in gray for a second
         tableView.deselectRow(at: indexPath, animated: true)
     
-        
     }
-    
+
     //MARK - 3. Add new items section
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -81,8 +105,11 @@ override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexP
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once user presses add item on uialert
+  
+         let newItem = Item()
+         newItem.title = textField.text!
             
-         self.itemArray.append(textField.text!)
+         self.itemArray.append(newItem)
             
          self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
@@ -101,7 +128,7 @@ override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexP
         
         
         
-    }
-    
 }
 
+
+}
---
+++ Do it/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Do it
+//
+//  Created by Prince Sonnenberg on 2019/01/06.
+//  Copyright © 2019 Prince Sonnenberg. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = " "
+    var done: Bool = false
+    
+}
